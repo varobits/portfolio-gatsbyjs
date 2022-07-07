fccf50fc71ec9caf7a6abdb8b39c17c5106ff678
Alvaro Aguero <a.aguerobonilla@gmail.com>
2022-07-07 14:02:06 +0200

    Contenido hero

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -58,29 +58,25 @@ const Hero = () => {
     return () => clearTimeout(timeout);
   }, []);
 
-  const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const one = <h1>Hola, mi nombre es</h1>;
+  const two = <h2 className="big-heading">Álvaro Aguero Bonilla.</h2>;
+  const three = <h3 className="big-heading">Creo proyectos en internet.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        Profesionalmente creo webs y hago que crezcan aumentando su tráfico online. Personalmente
+        intento aprender algo nuevo cada día.
       </p>
     </>
   );
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href="https://www.linkedin.com/in/alvaroaguero/"
       target="_blank"
-      rel="noreferrer">
-      Check out my course!
+      rel="noreferrer"
+    >
+      ¡Echá un ojo a mi Linkedin!
     </a>
   );
 

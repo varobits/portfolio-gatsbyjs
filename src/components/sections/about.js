d004a90ce58b95369445dd71c25b5c3080d6a05c
Alvaro Aguero <a.aguerobonilla@gmail.com>
2022-07-07 14:37:16 +0200

    Contenido about

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,43 +125,37 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['JavaScript (ES6+)', 'React', 'WordPress', 'PHP', 'SEO', 'Proyect Managament'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
-      <h2 className="numbered-heading">About Me</h2>
+      <h2 className="numbered-heading">Sobre mi</h2>
 
       <div className="inner">
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
-            </p>
-
-            <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
+              ¡Hola! Mi nombre es Álvaro y disfruto creando proyectos en internet. Soy Licenciado en
+              publicidad por la <a href="https://www.urjc.es/">URJC</a> y dispongo de formación en
+              Marketing Digital por{' '}
+              <a href="https://www.esic.edu/master-y-postgrado/master-en-digital-business-mdb">
+                ESIC
               </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              y en desarrollo web por <a href="https://github.com/Fictizia">FICTIZIA</a>. Además, me
+              encanta seguir aprendiendo de forma continua en comunidades onlie como{' '}
+              <a href="https://platzi.com/p/varobits/">Platzi</a>.
             </p>
 
-            <p>Here are a few technologies I’ve been working with recently:</p>
+            <p>
+              Actualmente me encuentro trabajando en Tinsa como Digital Project Manager, impulsando
+              y desarrollando diferentes proyectos online enfocados en digitalizar la compañía y
+              generar negocio por el canal online.
+            </p>
+
+            <p>
+              Aquí nombro algunas de las tecnologias y especialidades en las que he trabajado
+              recientemente:
+            </p>
           </div>
 
           <ul className="skills-list">
